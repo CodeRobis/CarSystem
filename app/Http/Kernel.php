63747ae30b390d7d3601f55b116967b5d0d98680
Robis <robertas.ruskys@gmail.com>
2023-03-21 21:33:05 +0200

    implemented shortCode

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            KeiksmazodziaiMiddleware::class,
             LanguageMiddleware::class,
         ],
 
@@ -69,7 +68,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'prisijunges' => PrisijungesMiddleware::class,
-        'suaugusiems'=>SuaugusiemsMiddleware::class
     ];
 }
