ff3173af273270436ff17cc129250e473161dea1
Robis <robertas.ruskys@gmail.com>
2023-05-24 16:43:07 +0300

    REST API added

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\OwnerController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('/owners', OwnerController::class);
+Route::apiResource('/cars', CarController::class);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -22,3 +26,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/duomenys', function (){
     return ["vardas"=>"Jonas", "pavarde"=>"Jonaitis"];
 });
+
